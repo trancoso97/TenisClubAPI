2eba1241cda0f1923dbda6cc388c6aeca082bc45
Rodrigo Trancoso <rdtrancoso@ibm.com>
2020-07-10 14:54:39 -0300

    Adição do route para disponibilidade

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 const ReservasRoute = require('./routes/reservasRoute')
+const DispRoute = require('./routes/disponibilidadeRoute');
 
 app.use('/reservas', ReservasRoute);
+app.use('/disponibilidade', DispRoute);
 
 module.exports = app;
 
---
+++ src/routes/disponibilidadeRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const Reservas = require('../models/reservasModel')
+
+router.get('/', async (req, res) => {
+
+})
+
+router.post('/', async (req, res) => {
+    
+})
+
+module.exports = router;
